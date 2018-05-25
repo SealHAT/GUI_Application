b4be225507c0c73533e09660cee8b00e30a91ffd
Huijie Pan <hpan5@ucsc.edu>
2018-05-25 13:55:04 -0700

    Changed constant power value

--- SealHAT_GUI_App/maindialog.h
+++ SealHAT_GUI_App/maindialog.h
@@ -9,38 +9,38 @@
 #include "seal_Types.h"
 
 
-#define	TOHOUR				3600
+#define	TOHOUR				3600.0
 #define I2C_Speed           1.0/400000
 #define SPI_SPEED           1.0/1000000
 
-#define TEMP_CONV_TIME  0.0108
-#define TEMP_CONV_PWR 0.00012
-#define TEMP_BIT_NUM        16
-#define TEMP_SB_PWR   0.00000062
-#define TEMP_I2C_PWR         0.004
+#define TEMP_CONV_TIME      7.0/1000
+#define TEMP_CONV_PWR       90/1000000
+#define TEMP_BIT_NUM        16.0
+#define TEMP_SB_PWR         0.06/1000000
+#define TEMP_I2C_PWR        3.5/1000
 
-#define LIGHT_BIT_NUM        16
-#define LIGHT_INACT_PWR   0.0000012
-#define LIGHT_ACT_PWR   0.0000016
+#define LIGHT_BIT_NUM       16.0
+#define LIGHT_INACT_PWR     0.65/1000000
+#define LIGHT_ACT_PWR       0.0000016
 
-#define IMU_SB_PWR  2.0/1000000
+#define IMU_SB_PWR          2.0/1000000
 
-#define EKG_I_AVDV  100/1000000
-#define EKG_I_OV    1.6/1000000
-#define EKG_I_SAVDV 2.5/1000000
-#define EKG_I_SOV   1.1/1000000
-#define EKG_OSCILLATOR  (1.4/1000000)*24
+#define EKG_I_AVDV          100.0/1000000
+#define EKG_I_OV            0.6/1000000
+#define EKG_I_SAVDV         0.51/1000000
+#define EKG_I_SOV           1.1/1000000
+#define EKG_OSCILLATOR      (1.4/1000000)*(24.0)
 
 #define floatDebug() qDebug() << fixed << qSetRealNumberPrecision(10)
 
 #define GPS_SB_PWR  20.0/1000000
 
-#define SPI_CURRENT     25.0/1000
-#define SPI_SB_CURRENT  50.0/1000000
+#define SPI_CURRENT     20.0/1000
+#define SPI_SB_CURRENT  15.0/1000000
 
-
-#define MICRO_ACT_PWR   31.0/1000000
+#define MICRO_ACT_PWR   32.0/1000000
 #define MICRO_SB_PWR    4.6/1000000
+
 namespace Ui {class maindialog;}
 
 class maindialog : public QDialog
